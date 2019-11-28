43b8681b52d3560204593f1493cee22d1dc26b92
Shaotao <shaotao1988@gmail.com>
2019-11-28 13:20:56 -0500

    thread_test
    Victor: Learning concurrent

---
+++ concurrent/thread_test.py
@@ -0,0 +1,37 @@
+import threading
+import time
+
+number = 0
+locker = threading.Lock()
+
+def timeit(func):
+    def wrapper(*args):
+        begain = time.time()
+        result = func(*args)
+        end = time.time()
+        print("time elapsed: ", end-begain)
+        return result
+    return wrapper
+
+class MyThread(threading.Thread):
+    @timeit
+    def run(self):
+        global number
+        for _ in range(1000000):
+            with locker:
+                number += 1
+        print(number)
+
+number1 = 0
+class MyThread1(threading.Thread):
+    @timeit
+    def run(self):
+        global number1
+        for _ in range(1000000):
+            number1 += 1
+        print(number1)
+
+if __name__ == '__main__':
+    for _ in range(5):
+        MyThread().start()
+        MyThread1().start()
